cfd1140ca10f14060d9567902c3db89a655b86f7
MorganeS117 <alexmoo@hotmail.fr>
2023-09-20 13:15:32 +0200

    espacevide

--- espaceVide.php
+++ espaceVide.php
@@ -1,22 +1,31 @@
 <?php
 
-echo "miamlemiel";
+//echo "miamlemiel <br>";
 
-echo "miamlemiel";
- $mot="espace ";
+
+ $mot="espac e                     ";
  $nouveau;
  $compteur = 0;
- $compteurDeux;
+ $compteurDeux =0;
 
 do {
     if ($mot[strlen($mot)-1] = ' ') {
-        while($compteur <> strlen($mot)-1-$compteurDeux){
+        while($compteur != strlen($mot)-1-$compteurDeux && $mot[$compteur] != " " ){
             $nouveau[$compteur] = $mot[$compteur];
             $compteur++;
         }
-        $compteurDeux++;
+        
     }
-} while ($nouveau[count($nouveau)-1] = ''); 
-echo $nouveau;
-echo"a";
+    $compteurDeux++;
+} while ($nouveau[count($nouveau)-1] = ' '); 
+
+
+
+for ($i=0; $i < count($nouveau); $i++) { 
+    
+
+    echo ($nouveau[$i]);
+}
+echo "a";
+
 ?>
