239b2310afef57f093dbc564e89459e750ae98f1
MorganeS117 <alexmoo@hotmail.fr>
2023-09-20 14:36:14 +0200

    PositionLettre

---
+++ PositionLettre.php
@@ -0,0 +1,7 @@
+<?php
+$alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+$lettre = "z";
+$compteur = 0;
+while ($lettre != $alpha[$compteur++]);
+echo("votre lettre est en position " . $compteur);
+?>
